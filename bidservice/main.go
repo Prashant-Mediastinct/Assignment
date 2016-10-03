6d7938433288c200dbabaa5ad6bc6f98be567d3f
Prashant-Mediastinct <prashant@mediastinct.com>
2016-10-03 17:50:47 +0530

    Mainly refactoring.. Changes to db definition and connection establishment. Its now onl created once before call the service and used throughout. Moved db querying ands fetching data to the database folder.

--- bidservice/main.go
+++ bidservice/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/Prashant-Mediastinct/Assignment/bidservice/controllers"
+	"github.com/Prashant-Mediastinct/Assignment/database"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	database.DBDef()
+	defer database.DB.Close()
 
 	http.HandleFunc("/publisher_id/", controllers.GetAdvertisers)
 	log.Fatal(http.ListenAndServe(":8081", nil))
