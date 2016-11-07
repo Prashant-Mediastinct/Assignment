fef0ae6ccd50df5207b304a01aae19e4600b3775
Prashant-Mediastinct <prashant@mediastinct.com>
2016-11-07 19:23:25 +0530

    updated .travis.yml

--- merge.sh
+++ merge.sh
@@ -6,7 +6,7 @@ fi
 
 if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "develop" ]; then
 echo "Merge to Develop"
-git checkout develop || exit 1
+git checkout "$TRAVIS_PULL_REQUEST_BRANCH" || exit 1
 git merge "$TRAVIS_COMMIT" || exit 1
 git push origin feature  # here need some authorization and url
 
