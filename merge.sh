290ecb9fe0d9faa0c1859f417f01f04187f172fc
Prashant-Mediastinct <prashant@mediastinct.com>
2016-11-07 20:35:05 +0530

    updated .travis.yml

--- merge.sh
+++ merge.sh
@@ -7,9 +7,12 @@ fi
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
 echo "Merge to Develop"
 git fetch
+echo "after fetch"
 git checkout "$TRAVIS_BRANCH" || exit 
+echo "after checkout"
 git merge "$TRAVIS_COMMIT" || exit 
-git push origin develop  # here need some authorization and url
+echo "after merge"
+git push origin "$TRAVIS_BRANCH"  # here need some authorization and url
 
 echo "Merging Complete!";
 fi
