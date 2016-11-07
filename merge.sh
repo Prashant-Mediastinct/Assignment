f7e512a0997f462ca6dcc5b408ed678de8350cfa
Prashant-Mediastinct <prashant@mediastinct.com>
2016-11-07 20:15:51 +0530

    updated a .travis.yml

--- merge.sh
+++ merge.sh
@@ -6,14 +6,9 @@ fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
 echo "Merge to Develop"
-<<<<<<< HEAD
-git checkout "$TRAVIS_BRANCH" || exit 1
-git merge "$TRAVIS_COMMIT" || exit 1
-=======
 git checkout "$TRAVIS_BRANCH" || exit 
 git merge "$TRAVIS_COMMIT" || exit 
->>>>>>> 100712af7d935a8dc67605a6df5b0fc706b20989
-git push origin feature  # here need some authorization and url
+git push origin develop  # here need some authorization and url
 
 echo "Merging Complete!";
 fi
