1991dd1d0793e46b42d7092a1053311d4c30b951
Prashant-Mediastinct <prashant@mediastinct.com>
2016-11-07 17:53:21 +0530

    updated .travis.yml

--- merge.sh
+++ merge.sh
@@ -3,7 +3,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] || [ "$TRAVIS_BRANCH" != "feature" ]; t
     exit 0;
 fi
 
-
 git checkout develop || exit
 git merge "$TRAVIS_COMMIT" || exit
 git push origin "$TRAVIS_PULL_REQUEST_BRANCH"  # here need some authorization and url
