57f0f95807402be8464b77c6c4f4c3727a7eb4e3
Prashant-Mediastinct <prashant@mediastinct.com>
2016-11-08 12:46:48 +0530

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -6,8 +6,8 @@ fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
 echo "Merge to Develop"
-git clone git@github.com:Prashant-Mediastinct/Assignment.git
-echo "after clone"
+git pull
+echo "after pull"
 git checkout "$TRAVIS_BRANCH" || exit 
 echo "after checkout"
 git merge "$TRAVIS_COMMIT" || exit 
